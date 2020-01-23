2775910d7804dd7c0877e6fc5ebc294f719025b9
Jaquan Piper <jaquan.piper@gmail.com>
2020-01-23 20:33:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,9 @@
 # Write a speak_to_grandma method.
 
-speak_to_grandma="I LOVE YOU GRANDMA!"
-
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN
-  
-  elsif"
-  
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU TOO PUMPKIN
+    end"
 end
-
 
   
   
